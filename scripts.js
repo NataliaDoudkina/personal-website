4e2d28466b627d7fda9cb91570cb00363a3d8fbe
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2019-09-14 12:41:12 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function displayResume(){
+    var resume = document.getElementById("resume");
+    if(resume.style.display=="none"){
+        resume.style.display="inline"}
+    else{
+        resume.style.display="none"
+    }
+}
