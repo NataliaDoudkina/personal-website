36bc0ec8bd4134563395e76aafa85634d4cb9d3c
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2020-04-09 18:01:50 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
-function displayResume(){
+/*function displayResume(){
     var resume = document.getElementById("resume");
     if(resume.style.display=="none"){
         resume.style.display="inline"}
     else{
         resume.style.display="none"
     }
-}
+}*/
+
+const btn = document.querySelector('displayResume')
+btn.addEventListener('click', function(){
+    const resume = document.getElementById("resume");
+    if(resume.style.display=="none"){
+        resume.style.display="inline"}
+    else{
+        resume.style.display="none"
+    }
+})
